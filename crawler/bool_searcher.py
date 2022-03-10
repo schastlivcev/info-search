23689211c4cc601188841ad9c30c1f66d0dc684a
Mikhail Schastlivcev <mikhail.schastlivcev@waveaccess.ru>
2022-03-10 20:02:14 +0300

    add task 3

---
+++ crawler/bool_searcher.py
@@ -0,0 +1,57 @@
+from nltk import WordNetLemmatizer
+from tokenizer import scan_nltk_packages
+
+
+INVERTED_INDEX_FILE_NAME = 'inverted_index.txt'
+INDEX_FILE_NAME = 'index.csv'
+AND = '&'
+OR = '|'
+NOT = '-'
+
+
+if __name__ == '__main__':
+    print("WikiHow bool search\n"
+          f"Available operations: '{OR}'(or), '{AND}'(and) and '{NOT}'(not) as the first character in the word.\n"
+          "Type '/q' to exit.")
+
+    scan_nltk_packages()
+    lemmatizer = WordNetLemmatizer()
+
+    num_of_pages = sum(1 for line in open(INDEX_FILE_NAME))
+    all_pages = set(range(1, num_of_pages))
+
+    index = {}
+    with open(INVERTED_INDEX_FILE_NAME) as index_file:
+        for line in index_file.readlines():
+            lemma_pages = line.split(' ')
+            index[lemma_pages[0]] = set(map(int, lemma_pages[1:]))
+
+    while True:
+        print('Your query: ', end='')
+        query = input().split(' ')
+        if query[0] == '/q':
+            break
+
+        words = query[::2]
+        operators = query[1::2]
+        operators.insert(0, OR)
+
+        result = set()
+        for word, operator in zip(words, operators):
+            if word.startswith(NOT):
+                search_word = word[1:]
+            else:
+                search_word = word
+
+            matched_pages = index.get(lemmatizer.lemmatize(search_word), set())
+            if word.startswith(NOT):
+                matched_pages = all_pages.difference(matched_pages)
+            # show pages for each word
+            # print(f'{word}: {sorted(matched_pages)}')
+
+            if operator == OR:
+                result.update(matched_pages)
+            elif operator == AND:
+                result = result.intersection(matched_pages)
+
+        print(f'Suitable pages: {sorted(result)}')
---
+++ crawler/indexer.py
@@ -0,0 +1,35 @@
+import os
+
+
+PAGES_DIRECTORY = 'pages'
+PAGE_PREFIX = 'page_'
+LEMMAS_FILE_NAME = 'lemmas.txt'
+INVERTED_INDEX_FILE_NAME = 'inverted_index.txt'
+
+
+def create_inverted_index(lemmas_map):
+    inverted_index = {}
+    for file in os.listdir(PAGES_DIRECTORY):
+        with open(f'{PAGES_DIRECTORY}/{file}', 'r', encoding='utf-8') as page:
+            page_content = page.read()
+            for lemma, tokens in lemmas_map.items():
+                for token in tokens:
+                    if token in page_content:
+                        pages = inverted_index.get(lemma, set())
+                        pages.add(file[len(PAGE_PREFIX):file.find('.')])
+
+                        inverted_index[lemma] = pages
+    return inverted_index
+
+
+if __name__ == '__main__':
+    lemmas_map = {}
+    with open(LEMMAS_FILE_NAME) as lemmas_file:
+        for line in lemmas_file.readlines():
+            values = line.strip().split(' ')
+            lemmas_map[values[0].replace(':', '')] = values[1:]
+
+    inverted_index = create_inverted_index(lemmas_map)
+    with open(INVERTED_INDEX_FILE_NAME, 'w', encoding='utf-8') as inverted_index_file:
+        for lemma, pages in inverted_index.items():
+            inverted_index_file.write(f'{lemma} {" ".join(sorted(pages, key=int))}\n')
